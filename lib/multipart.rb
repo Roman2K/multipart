d84c1181bdfc485af9bc822f1dfec9d51485cbf7
Roman Le Négrate <roman.lenegrate@gmail.com>
2014-07-15 14:11:58 +0200

    initial commit

---
+++ lib/multipart.rb
@@ -0,0 +1,98 @@
+require 'uri'
+require 'securerandom'
+
+class Multipart
+  EOL = "\r\n"
+
+  def initialize(params)
+    @params = ParamsNormalizer.new(params)
+    @boundary = SecureRandom.uuid
+  end
+
+  attr_reader :boundary
+
+  include Enumerable
+
+  def content_type
+    "multipart/form-data; boundary=#{@boundary}"
+  end
+
+  def each
+    @params.each do |key, value|
+      unless value.respond_to?(:to_str) || value.respond_to?(:each)
+        value = value.to_s 
+      end
+      chunk = "--#{@boundary}"
+      dispos = ["Content-Disposition: form-data", "name=\"#{key}\""]
+      dispos << "filename=\"#{value.filename}\"" if value.kind_of? File
+      chunk << EOL << dispos.join('; ')
+      chunk << EOL << EOL
+      if value.respond_to? :to_str
+        chunk << value << EOL
+        yield chunk
+      else
+        yield chunk
+        value.each { |c| yield c }
+        yield EOL
+      end
+    end
+    yield "--#{@boundary}--"
+  end
+
+  def to_s
+    to_a.join
+  end
+
+  class ParamsNormalizer
+    def initialize(params)
+      @params = params
+    end
+
+    include Enumerable
+
+    def each
+      @params.kind_of? Hash or raise "params must be a Hash"
+      yield_nested = lambda do |value, key|
+        case value
+        when Hash
+          value.each do |k,v|
+            escaped_key = if key
+              "#{key}[#{URI.encode(k.to_s)}]"
+            else
+              URI.encode(k.to_s)
+            end
+            yield_nested.call(v, escaped_key)
+          end
+        when Array
+          k = "#{key}[]"
+          value.each do |el|
+            yield_nested.call(el, k)
+          end
+        else
+          yield key, value
+        end
+      end
+      yield_nested.call(@params, nil)
+    end
+  end
+
+  class File
+    BUFFER_SIZE = 128 * 1024 * 1024
+
+    def initialize(path)
+      @path = path
+    end
+
+    def filename
+      ::File.basename @path
+    end
+
+    def each
+      ::File.open(@path, 'rb') do |f|
+        while chunk = f.read(BUFFER_SIZE)
+          yield chunk
+        end
+      end
+    end
+  end
+end
---
+++ test/rack_parse.rb
@@ -0,0 +1,43 @@
+$:.unshift __dir__ + '/../lib'
+
+require 'minitest/autorun'
+require 'rack'
+require 'multipart'
+
+class MultipartRackParseTest < MiniTest::Unit::TestCase
+  def test_simple
+    assert_equal({"a" => "OK"}, parse({a: "OK"}))
+  end
+
+  def test_nested
+    assert_equal({"a" => {"test" => "OK"}}, parse({a: {test: "OK"}}))
+    assert_equal({"a" => ["OK"]}, parse({a: ["OK"]}))
+  end
+
+  def test_file
+    result = parse({a: Multipart::File.new(__dir__ + '/file')})
+    assert_equal "file", result["a"][:filename]
+    assert_equal "test contents\n", result["a"][:tempfile].read
+  end
+
+  def test_mixed
+    result = parse({a: Multipart::File.new(__dir__ + '/file'), b: "OK"})
+    assert_equal "file", result["a"][:filename]
+    assert_equal "OK", result["b"]
+
+    result = parse({a: "OK", b: Multipart::File.new(__dir__ + '/file')})
+    assert_equal "OK", result["a"]
+    assert_equal "file", result["b"][:filename]
+  end
+
+private
+
+  def parse(params)
+    multipart = Multipart.new(params)
+    input = StringIO.new(multipart.to_s)
+    Rack::Multipart.parse_multipart({
+      'CONTENT_TYPE' => multipart.content_type,
+      'rack.input' => input,
+    })
+  end
+end
